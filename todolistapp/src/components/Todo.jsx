d94f1764f66ea870c7c835e7a81f3fb43d01acd8
Igho-Iggue Iruesiri <ambassadorj.boy@gmail.com>
2024-02-26 13:31:56 +0100

    2-23-24 adding footer componenet

--- todolistapp/src/components/Todo.jsx
+++ todolistapp/src/components/Todo.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react"
 import Form from "./Form";
 import TodoList from "./TodoList";
+import Footer from "./Footer";
 
 //component to set todolist items
 export default function Todo(){
 
     //set array to stack todolistd items
     const [todos, setTodos] = useState([]);
+
+    //count the total number of completed todos
+    const completedTodos = todos.filter((todo)=>todo.done).length;
+
+    //count the total number of todos
+    const totalTodos = todos.length
 
     return(
         <div>
@@ -17,6 +24,9 @@ export default function Todo(){
             {/*Todolist component for the display of todolist items*/}
             <TodoList todos={todos} setTodos={setTodos}/>
 
+            {/*Footer componenet*/}
+            <Footer completedTodos={completedTodos} totalTodos={totalTodos}/>
         </div>
+
     )
 }
